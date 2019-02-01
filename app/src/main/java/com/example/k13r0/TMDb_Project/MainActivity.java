cb3177f62c0bc2b4c1c7da94f5b170c1becf2cfb
kghiggs <43546390+kghiggs@users.noreply.github.com>
2019-02-01 13:00:14 -0500

    Random Page Added Part 2 - WIP - NON-FUNCTIONAL

--- app/src/main/java/com/example/k13r0/TMDb_Project/MainActivity.java
+++ app/src/main/java/com/example/k13r0/TMDb_Project/MainActivity.java
@@ -9,8 +9,6 @@ import android.app.Activity;
 import android.view.View;
 
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.android.volley.toolbox.Volley;
 import com.android.volley.RequestQueue;
