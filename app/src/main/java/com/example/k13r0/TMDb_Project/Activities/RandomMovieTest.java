d204316e2428798f86641119887e2bfc22c761fd
kghiggs <43546390+kghiggs@users.noreply.github.com>
2019-02-01 12:05:41 -0500

    Random Page Added - WIP - NON-FUNCTIONAL

--- app/src/main/java/com/example/k13r0/TMDb_Project/Activities/RandomMovieTest.java
+++ app/src/main/java/com/example/k13r0/TMDb_Project/Activities/RandomMovieTest.java
@@ -1,9 +1,23 @@
 package com.example.k13r0.TMDb_Project.Activities;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+import com.example.k13r0.TMDb_Project.Classes.Movie;
+import com.example.k13r0.TMDb_Project.Classes.Session;
+import com.example.k13r0.TMDb_Project.MainActivity;
 import com.example.k13r0.TMDb_Project.R;
+import com.squareup.picasso.Picasso;
+
+import java.text.SimpleDateFormat;
 
 public class RandomMovieTest extends AppCompatActivity
 {
@@ -11,6 +25,44 @@ public class RandomMovieTest extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
+        setContentView(R.layout.activity_random);
+
+        final Context context = getApplicationContext();
+        final RequestQueue requestQueue = Volley.newRequestQueue(this);
+        final Session guestSession = new Session(context);
+
+        final TextView randomMovieTitle = findViewById(R.id.randomTitle);
+        final TextView randomOverview = findViewById(R.id.randomOverview);
+        final ImageView randomPoster = findViewById(R.id.randomPoster);
+        String titleAndYear = "";
+
+        Button randomButton = findViewById(R.id.searchButton);
+        randomButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Movie.GetRandomMovie(guestSession, requestQueue);
+                Movie randomMovie = guestSession.GetRandomMovieObject();
+
+                randomMovieTitle.setText(randomMovie.title);
+                randomOverview.setText(randomMovie.overview);
+
+                if (randomMovie.releaseDate != null)
+                {
+                    SimpleDateFormat year = new SimpleDateFormat("yyyy");
+                    String titleAndYear = randomMovie.title + " (" + year.format(randomMovie.releaseDate) + ")";
+                    randomMovieTitle.setText(titleAndYear);
+                }
+
+
+                //TextView randomReleaseDate = findViewById(R.id.randomReleaseDate);
+                //randomReleaseDate.setText(randomMovie.releaseDate.toString());
+                if (randomMovie.posterPath != null)
+                {
+                    Picasso.with(context).load("https://image.tmdb.org/t/p/w500/" + randomMovie.posterPath).into(randomPoster);
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/k13r0/TMDb_Project/MainActivity.java
+++ app/src/main/java/com/example/k13r0/TMDb_Project/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.k13r0.TMDb_Project;
 
-import android.support.constraint.ConstraintLayout;
-import android.support.design.widget.Snackbar;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Context;
@@ -11,21 +10,18 @@ import android.view.View;
 
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.support.constraint.ConstraintLayout;
+
 import com.android.volley.toolbox.Volley;
 import com.android.volley.RequestQueue;
+
 import com.example.k13r0.TMDb_Project.Activities.About;
 import com.example.k13r0.TMDb_Project.Activities.SearchMovies;
 import com.example.k13r0.TMDb_Project.Activities.UpcomingMovies;
+import com.example.k13r0.TMDb_Project.Activities.RandomMovieTest;
+
 import com.example.k13r0.TMDb_Project.Classes.Session;
-import com.example.k13r0.TMDb_Project.Classes.Movie;
-import com.example.k13r0.TMDb_Project.Classes.MakeSnackbar;
 
-import com.squareup.picasso.Picasso;
-
-import java.text.SimpleDateFormat;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -49,27 +45,10 @@ public class MainActivity extends AppCompatActivity
         final Context context = getApplicationContext();
         final Session guestSession = new Session(context);
 
-        TextView randomMovieTitle = findViewById(R.id.randomTitle);
-        TextView randomOverview = findViewById(R.id.randomOverview);
-        final ImageView randomPoster = findViewById(R.id.randomPoster);
-
         guestSession.SetAPIKey("27fc45ada92ace137689b2c3ddb1c6d0");
         guestSession.GetGSID(requestQueue);
 
-        Movie.GetRandomMovie(guestSession, requestQueue);
-        Movie randomMovie = guestSession.GetRandomMovieObject();
-        if (randomMovie.releaseDate != null)
-        {
-            SimpleDateFormat year = new SimpleDateFormat("yyyy");
-            String titleAndYear = randomMovie.title + " (" + year.format(randomMovie.releaseDate) + ")";
-            randomMovieTitle.setText(titleAndYear);
-        }
 
-
-        if (randomMovie.posterPath != null)
-        {
-            Picasso.with(context).load("https://image.tmdb.org/t/p/w500/" + randomMovie.posterPath).into(randomPoster);
-        }
 
 
         // final Bitmap bitmap = BitmapFactory.decodeFile("/sdcard/image.jpg");
@@ -149,26 +128,7 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-                Movie.GetRandomMovie(guestSession, requestQueue);
-                Movie randomMovie = guestSession.GetRandomMovieObject();
-                TextView randomMovieTitle = findViewById(R.id.randomTitle);
-                randomMovieTitle.setText(randomMovie.title);
-                TextView randomOverview = findViewById(R.id.randomOverview);
-                randomOverview.setText(randomMovie.overview);
-
-                if (randomMovie.releaseDate != null)
-                {
-                    SimpleDateFormat year = new SimpleDateFormat("yyyy");
-                    String titleAndYear = randomMovie.title + " (" + year.format(randomMovie.releaseDate) + ")";
-                    randomMovieTitle.setText(titleAndYear);
-                }
-
-                //TextView randomReleaseDate = findViewById(R.id.randomReleaseDate);
-                //randomReleaseDate.setText(randomMovie.releaseDate.toString());
-                if (randomMovie.posterPath != null)
-                {
-                    Picasso.with(context).load("https://image.tmdb.org/t/p/w500/" + randomMovie.posterPath).into(randomPoster);
-                }
+                startActivity(new Intent(MainActivity.this, RandomMovieTest.class));
             }
         });
     }
