861acdeb4f205b7d7367d9596464b6771f9c92d1
Arie Kraayenbrink <20170212+akraa@users.noreply.github.com>
2019-01-31 20:46:57 -0500

    Add button click event handler.

--- app/src/main/java/com/example/k13r0/TMDb_Project/Activities/SearchMovies.java
+++ app/src/main/java/com/example/k13r0/TMDb_Project/Activities/SearchMovies.java
@@ -4,15 +4,18 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.k13r0.TMDb_Project.R;
 
-public class SearchMovies extends AppCompatActivity
+public class SearchMovies extends AppCompatActivity implements View.OnClickListener
 {
     private EditText txtMovieSearch;
     private Button btnSearch;
+    private TextView txtResults;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +25,11 @@ public class SearchMovies extends AppCompatActivity
 
         txtMovieSearch = findViewById(R.id.txtSearch);
         btnSearch = findViewById(R.id.btnSearch);
+        txtResults = findViewById(R.id.txtSearchResults);
 
         txtMovieSearch.addTextChangedListener(searchTextWatcher);
+
+        btnSearch.setOnClickListener(this);
     }
 
     private TextWatcher searchTextWatcher = new TextWatcher() {
@@ -44,4 +50,10 @@ public class SearchMovies extends AppCompatActivity
 
         }
     };
+
+
+    @Override
+    public void onClick(View v) {
+        txtResults.setVisibility(View.VISIBLE);
+    }
 }
