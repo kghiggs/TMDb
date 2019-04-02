d6a16c34aa6ba3308d7ef695f8e1ad44485f3b8a
Bailey Mills <bmills1928@conestogac.on.ca>
2019-04-02 16:58:36 -0400

    Preparing to load movie details when clicked

--- app/src/main/java/com/example/k13r0/TMDb_Project/Activities/MovieDetails.java
+++ app/src/main/java/com/example/k13r0/TMDb_Project/Activities/MovieDetails.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
@@ -11,11 +13,18 @@ import com.example.k13r0.TMDb_Project.R;
 import com.example.k13r0.TMDb_Project.Utilities.Session;
 import com.example.k13r0.TMDb_Project.Utilities.Movie;
 
+import static com.example.k13r0.TMDb_Project.Utilities.MakeSnackbar.ShowSnackbar;
+
 public class MovieDetails extends AppCompatActivity
 {
     private Context context;
     private RequestQueue requestQueue;
     private Session guestSession;
+
+    private TextView txtTitle;
+    private TextView txtDetails;
+    private ImageView imgPoster;
+    private ImageView imgBackground;
 
     /*
      * Function		: onCreate
@@ -31,4 +40,21 @@ public class MovieDetails extends AppCompatActivity
         Bundle data = getIntent().getExtras();
         Movie selectedMovie = (Movie) data.getParcelable("selectedMovie");
     }
+
+
+    /*
+     * Function		: onCreate
+     * Description	: Requests a random page from the database and displays information relating to it; allows user to
+     *                repeat the process with the press of a button.
+     * Parameters	: View v : the view to create the snackbar notification for
+     * Returns		: N/A
+     */
+    private void LoadRandomMovie(View v){
+        Movie.GetRandomMovie(guestSession, requestQueue, context);
+        Movie randomMovie = guestSession.GetRandomMovieObject(context);
+
+        
+
+    }
+
 }
