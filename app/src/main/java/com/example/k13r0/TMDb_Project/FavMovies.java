cfde2fd001c06a463785a517e7bad73b0d0cee39
Arie Kraayenbrink <20170212+akraa@users.noreply.github.com>
2019-04-03 09:19:37 -0400

    Add FavMoviesDBHelper class for SQLite

--- app/src/main/java/com/example/k13r0/TMDb_Project/FavMovies.java
+++ app/src/main/java/com/example/k13r0/TMDb_Project/FavMovies.java
@@ -1,4 +1,16 @@
 package com.example.k13r0.TMDb_Project;
 
+import android.provider.BaseColumns;
+
 public class FavMovies {
+
+    private FavMovies(){}
+
+    public static final class MovieEntry implements BaseColumns
+    {
+        public static final String TABLE_NAME = "favMovies";
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_ID = "id";
+        public static final String COLUMN_TIMESTAMP = "timestamp";
+    }
 }
---
+++ app/src/main/java/com/example/k13r0/TMDb_Project/FavMoviesDBHelper.java
@@ -0,0 +1,16 @@
+package com.example.k13r0.TMDb_Project;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class FavMoviesDBHelper extends SQLiteOpenHelper {
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
